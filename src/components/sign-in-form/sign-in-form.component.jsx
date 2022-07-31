0daa5b5e71d90628c4a6dc9af775262e67413e26
Lukasz Swiderski <lukasz@swiderski.xyz>
2022-07-31 14:06:13 +0200

    converted Button to styled-component with object literal switch

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -11,7 +11,7 @@ import {
 } from "../../utils/firebase/firebase.utils";
 
 import FormInput from '../../components/form-input/form-input.component'
-import Button from "../button/button.component";
+import Button, { BUTTON_TYPE_CLASSES } from "../button/button.component";
 
 import './sign-in-form.styles.scss'
 
@@ -95,7 +95,7 @@ const SignInForm = () => {
 
                 <div className="buttons-container">
                     <Button type="submit">Sign In</Button>
-                    <Button type="button" buttonType="google" onClick={signInWithGoogle}>
+                    <Button type="button" buttonType={BUTTON_TYPE_CLASSES.google} onClick={signInWithGoogle}>
                         Google Sign-in</Button>
                     {/*
                         <Button buttonType="google" onClick={signInWithGoogleRedirect}>
